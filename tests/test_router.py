964441aea31c9b76aecb32d7e291e051a0da2727
Ankur  Shrivastava <ankurshrivastava@Ankurs-Mac-mini.local>
2026-06-13 23:00:06 +0530

    add test for get order

--- tests/test_router.py
+++ tests/test_router.py
@@ -1,5 +1,13 @@
+from typing import TYPE_CHECKING
 from unittest.mock import MagicMock, patch
 from uuid import UUID
+
+import pytest
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+    from sqlalchemy.orm import Session
 
 from sqlalchemy.exc import IntegrityError
 
@@ -35,13 +43,28 @@ def test_create_user_raises_integrity_error(test_client):
         app.dependency_overrides.clear()
 
 
-def test_create_order(test_client, create_tables, set_environment):  # noqa: ARG001
+@pytest.fixture
+def db_session(set_environment) -> Generator[Session]:  # noqa: ARG001
     settings = get_app_settings()
     engine = get_engine(settings)
-    session = next(get_database_session(engine))
+    gen = get_database_session(engine)
+    session = next(gen)
+    try:
+        yield session
+    finally:
+        gen.close()
+
+
+def test_create_order(test_client, create_tables, set_environment, db_session):  # noqa: ARG001
+    """
+    Tests order create endpoint.
+
+    Creates User object and uses it to create a order.
+    Overrides get broker depedency to use mock broker.
+    """
     user = User(name="test_user2")
-    session.add(user)
-    session.commit()
+    db_session.add(user)
+    db_session.commit()
     mock_broker = MagicMock()
     app.dependency_overrides[get_message_broker] = lambda: mock_broker
     try:
@@ -50,7 +73,7 @@ def test_create_order(test_client, create_tables, set_environment):  # noqa: ARG
             json={"customer_name": "test_user2", "amount": 1000, "status": "pending"},
         )
         assert response.status_code == 201, response.json()  # noqa: PLR2004, S101
-        order = session.get(Order, UUID(response.json()["id"]))
+        order = db_session.get(Order, UUID(response.json()["id"]))
         assert order is not None  # noqa: S101
         assert order.customer_id == user.id  # noqa: S101
         mock_broker.send.assert_called_once_with(
@@ -58,5 +81,21 @@ def test_create_order(test_client, create_tables, set_environment):  # noqa: ARG
             body={"order_id": str(response.json()["id"])},
         )
     finally:
-        session.close()
         app.dependency_overrides.clear()
+
+
+def test_get_order(test_client, create_tables, db_session):
+    user = User(name="test_user3")
+    db_session.add(user)
+    db_session.flush()
+    order = Order(customer_id=user.id, amount=2000, status="pending")
+    db_session.add(order)
+    db_session.commit()
+    order_id = order.id
+    response = test_client.get(f"/order/{order_id}")
+    assert response.status_code == 200, response.json()
+    data = response.json()
+    assert data["id"] == str(order.id)
+    assert data["customerId"] == str(user.id)
+    assert data["amount"] == 2000
+    assert data["status"] == "pending"
